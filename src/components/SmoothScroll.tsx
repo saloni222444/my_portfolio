c2f14d2233423d1c03146585b2e92cff06e6f3e8
Saloni <146350330+saloni222444@users.noreply.github.com>
2026-05-07 18:34:08 +0000

    update

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
